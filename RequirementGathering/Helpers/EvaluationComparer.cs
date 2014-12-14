bbe5f7a0fd580dedcfe0831d158d16df953e9960
zhangcong2711 <zhangcong2711@outlook.com>
2014-12-14 22:58:23 +0200

    unit test for evaluation; sorting on product and evaluation

---
+++ RequirementGathering/Helpers/EvaluationComparer.cs
@@ -0,0 +1,89 @@
+﻿using RequirementGathering.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RequirementGathering.Helpers
+{
+    public class EvaluationComparer : IComparer<Evaluation>
+    {
+        protected string sortName;
+        protected bool isAsc;
+
+        public EvaluationComparer(string sortName, bool isAsc)
+        {
+            this.sortName = sortName;
+            this.isAsc = isAsc;
+        }
+
+        public int Compare(Evaluation x, Evaluation y)
+        {
+
+            if (sortName.Equals("ProductName"))
+            {
+
+                if (isAsc)
+                {
+                    if (x.Product.Name == null)
+                    {
+                        return -1;
+                    }
+                    return (x.Product.Name.CompareTo(y.Product.Name));
+                }
+                else
+                {
+                    if (y.Product.Name == null)
+                    {
+                        return 1;
+                    }
+                    return (y.Product.Name.CompareTo(x.Product.Name));
+                }
+            }
+
+            if (sortName.Equals("Description"))
+            {
+
+                if (isAsc)
+                {
+                    if (x.Description == null)
+                    {
+                        return -1;
+                    }
+                    return (x.Description.CompareTo(y.Description));
+                }
+                else
+                {
+                    if (y.Description == null)
+                    {
+                        return 1;
+                    }
+                    return (y.Description.CompareTo(x.Description));
+                }
+            }
+
+            if (sortName.Equals("Version"))
+            {
+                if (isAsc)
+                {
+                    if (x.Version == null)
+                    {
+                        return -1;
+                    }
+                    return (x.Version.CompareTo(y.Version));
+                }
+                else
+                {
+                    if (y.Version == null)
+                    {
+                        return 1;
+                    }
+                    return (y.Version.CompareTo(x.Version));
+                }
+            }
+
+
+            return (x.ToString().CompareTo(y.ToString()));
+        }
+    }
+}
